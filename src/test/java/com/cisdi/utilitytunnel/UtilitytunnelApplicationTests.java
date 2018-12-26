1fbb321acae35d9ca775c379bb328eea220d1790
邓向敏 <1414372114@qq.com>
2018-12-26 13:33:02 +0800

    1.0.0 12/26/2018

---
+++ src/test/java/com/cisdi/utilitytunnel/UtilitytunnelApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cisdi.utilitytunnel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UtilitytunnelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
